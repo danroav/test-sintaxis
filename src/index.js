581de135fd19831b20acddc48318895835b9bdd1
danroav <danielon83@gmail.com>
2021-08-10 18:59:25 -0400

    Pruebas iniciales

--- src/index.js
+++ src/index.js
@@ -8,3 +8,53 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+//En los casos que sea posible mostrar en consola el tipo de datos
+
+//Escribir comentarios de 2 formas
+
+//Definir una variable
+
+//Mostrar en consola y definir un arreglo de 2 formas
+
+//Mostrar en consola y definir objetos de 3 formas
+
+//Mostrar en consola y definir cadenas de 2 formas
+
+//Mostrar en consola y definir cadenas en multiples lineas de
+
+//Mostrar en consola y definir una cadena con caracteres especiales \,',"
+
+//Mostrar en consola dos variables con los nombres [minombre] es igual a [Minombre]
+
+//Escribir un comentario si el nombre de una variable puede comenzar con un numero
+
+//Mostrar en consola el valor de una variable del tipo entero
+
+//Mostrar en consola y definir un numero double y un numero double menor a 1
+
+//Mostrar en consola y definir un numero a la potencia de 10
+
+//Monstrar en consola y definir un numero BigInt
+
+//Mostrar en consola y definir una variable booleana
+
+//Declarar una variable con valor indefinido de 2 formas
+
+//Declarar una variable con valor null
+
+//Declarar una funcion llamarA[Apellido] que devuelva [Apellido]
+
+//Mostrar en consola y declarar una variable con referencia a una funcion escribirEnConsola(texto) que escriba en la consola
+
+//Mostrar en consola y declarar una funcion que calcule la potencia de una base y un exponente
+
+//Mostrar en consola y declarar una funcion que calcule si un numero es par o impar
+
+//Mostrar en consola y declarar el resultado de una operacion ternaria
+
+//Mostrar en consola y declarar el resultado de una sentencia selectiva de 2 formas
+
+//Mostrar en consola y declarar el resultado de una sentencia repetitiva de 4 formas
+
+//Crear una clase Persona con las propiedades nombre y apellido y un metodo nombreCompleto que concatene las dos propiedades
+//Instanciar la clase con el nombre del alumno y mostrar el resultado en consola
