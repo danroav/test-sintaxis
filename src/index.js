8b37012196eda951508d85479c5765c8bf0de672
danroav <danielon83@gmail.com>
2021-08-11 15:32:24 -0400

    Llenado de los 30 ejercicios

--- src/index.js
+++ src/index.js
@@ -8,53 +8,108 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
-//En los casos que sea posible mostrar en consola el tipo de datos
+//Al mostrar en consola mostrar el resultado y también el tipo de datos del resultado
 
-//Escribir comentarios de 2 formas
+//Utilizar nombres de variables y funciones en terminos significativos, es decir
+//evitar nombrar las variables a, b, x, i, j sino mas bien indice, listaDeItems,
+//numeroElementos
 
-//Definir una variable
+/*
+IDENTIFICADORES, VARIABLES Y TIPOS DE DATOS
+ */
+//1. Escribir comentarios de 2 formas
 
-//Mostrar en consola y definir un arreglo de 2 formas
+//2. Definir una variable, de 2 formas y definir una constante
 
-//Mostrar en consola y definir objetos de 3 formas
+//3. Mostrar en consola y definir un arreglo de 2 formas
 
-//Mostrar en consola y definir cadenas de 2 formas
+//4. Mostrar en consola y definir objetos de 3 formas
 
-//Mostrar en consola y definir cadenas en multiples lineas de
+//5. Mostrar en consola y definir cadenas de 2 formas
 
-//Mostrar en consola y definir una cadena con caracteres especiales \,',"
+//6. Mostrar en consola y definir cadenas en multiples lineas de
 
-//Mostrar en consola dos variables con los nombres [minombre] es igual a [Minombre]
+//7. Mostrar en consola y definir una cadena con caracteres especiales \,',"
 
-//Escribir un comentario si el nombre de una variable puede comenzar con un numero
+//8. Declarar dos variables con los nombres [minombre] es igual a [Minombre] con
+// valores de numeros diferentes y mostrar en consola la siguiente expresion
+// [minombre]==[minombre]
 
-//Mostrar en consola el valor de una variable del tipo entero
+//9. Mostrar en consola un texto de Cierto o Falso si el nombre de una variable
+//puede comenzar con un numero
 
-//Mostrar en consola y definir un numero double y un numero double menor a 1
+//10. Mostrar en consola y definir el valor de una variable del tipo entero
 
-//Mostrar en consola y definir un numero a la potencia de 10
+//11. Mostrar en consola y definir el valor de una variable del tipo double y un
+//numero double menor a 1
 
-//Monstrar en consola y definir un numero BigInt
+//12. Mostrar en consola y definir el valor de una variable numero con el formato de
+//exponente base 10
 
-//Mostrar en consola y definir una variable booleana
+//13. Monstrar en consola y definir un numero BigInt
 
-//Declarar una variable con valor indefinido de 2 formas
+//14. Mostrar en consola y definir dos variable booleanas con los posibles valores
+//booleanos
 
-//Declarar una variable con valor null
+//15. Mostrar en consola y declarar una variable con valor indefinido
 
-//Declarar una funcion llamarA[Apellido] que devuelva [Apellido]
+//16. Mostrar en consola y declarar una variable con valor nulo
 
-//Mostrar en consola y declarar una variable con referencia a una funcion escribirEnConsola(texto) que escriba en la consola
+//17. Declarar una funcion llamarA[Apellido] que devuelva [Apellido]
 
-//Mostrar en consola y declarar una funcion que calcule la potencia de una base y un exponente
+//18. Declarar una variable con referencia a la funcion definida
+//anteriormente (ejercicio 17)  y mostrar en consola la ejecucion de esa variable
 
-//Mostrar en consola y declarar una funcion que calcule si un numero es par o impar
+//19. Declarar una variable con la fecha de hoy, agregar 30 dias a la fecha y
+//mostrar en consola el resultado
 
-//Mostrar en consola y declarar el resultado de una operacion ternaria
+//20. Declarar una funcion probarArgumentos(argumentos) que utilice el objeto
+//arguments de la función para mostrar en consola todos los valores de argumentos
+//concatenados. Mostrar en consola el siguiente caso probarArgumentos(6,"abc",true)
 
-//Mostrar en consola y declarar el resultado de una sentencia selectiva de 2 formas
+//21. Declarar una funcion agregarElementoArray(arreglo) que reciba un arreglo de
+//argumentos y muestre en consola la longitud del arreglo, luego que
+//agregue tres elementos "aaa", 12, {x:true},.45,1E10 al arreglo, luego que muestre
+//en consola el contenido del arreglo y luego que quite el ultimo elemento de la
+//lista, luego que quite el elemento en el indice 2 y finalmente muestre en
+//consola el arreglo resultante
 
-//Mostrar en consola y declarar el resultado de una sentencia repetitiva de 4 formas
+/*
+OPERADORES ARITMETICOS Y LOGICOS
+ */
+//22. Mostrar en consola y declarar una funcion que calcule la potencia de una base
+//y un exponente
 
-//Crear una clase Persona con las propiedades nombre y apellido y un metodo nombreCompleto que concatene las dos propiedades
-//Instanciar la clase con el nombre del alumno y mostrar el resultado en consola
+//23. Mostrar en consola y declarar una funcion que calcule si un numero es par o
+//impar
+
+//24. Mostrar en consola y declarar una funcion con 3 parametros de distinto tipo
+// que devuelva la concatenación de esos parametros, de 2 formas posibles
+
+//25. Mostrar en consola y declarar el resultado de una operacion ternaria
+
+/*
+SENTENCIAS SELECTIVAS Y REPETITIVAS
+*/
+//26. Mostrar en consola y declarar el resultado de una sentencia selectiva
+//de 2 formas posibles
+
+//27. Mostrar en consola y declarar el resultado de una sentencia repetitiva
+//de 4 formas posibles
+
+/*
+TIPOS DE REFERENCIA
+*/
+
+//28. Crear una clase Persona con un constructor que reciba el nombre y apellido
+//las propiedades nombre y apellido y un metodo nombreCompleto que concatene las dos propiedades
+
+//29. Instanciar la clase Persona con el nombre de usted como alumno y mostrar el resultado en consola
+
+/*
+FUNCIONES ANONIMAS
+*/
+
+//30. Declarar una variables con una funcion anonima con un parametro y
+// otra funcion anonima con 2 parametros, mostrar los resultados
+// de ejecutar ambas variables
