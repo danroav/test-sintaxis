5db8e2cc10d17a118a696bc36f4f74df095cb466
danroav <danielon83@gmail.com>
2021-08-12 14:34:19 -0400

    Clase 4 Preparar Nombres Practica

--- src/index.js
+++ src/index.js
@@ -56,7 +56,8 @@ console.log(cadena);
 var cad = String("Hola");
 console.log(cad);
 //6. Mostrar en consola y definir cadenas en multiples
-//lineas de Mauricio Aramayo/ Erlan Mendoza
+//lineas
+// Mauricio Aramayo / Erlan Mendoza
 var x =
   "hola mundooooooooooooooooo" + "oooooooooooooooooooooooooooooooooooooooooo ";
 console.log(x);
@@ -67,8 +68,7 @@ var cadena2 = "sssssssssssssssssssssss\
 sssss";
 console.log(cadena2);
 //7. Mostrar en consola y definir una cadena con caracteres especiales \,',"
-//Joan Saravia
-//Kevin Acosta
+//Joan Saravia / Kevin Acosta
 var cadena = '"hola"';
 var cadenaescape1 = " \\ Escape";
 var cadenaescape2 = " 'esto es una comilla simple'";
@@ -117,22 +117,29 @@ var num = 20n;
 console.log(num.toString());
 //14. Mostrar en consola y definir dos variable booleanas con los posibles valores
 //booleanos
+// Jhon Rodrigo Acuña
 
 //15. Mostrar en consola y declarar una variable con valor indefinido
+// Oscar Aguilar
 
 //16. Mostrar en consola y declarar una variable con valor nulo
+// Mauricio Aramayo
 
 //17. Declarar una funcion llamarA[Apellido] que devuelva [Apellido]
+//  Carlos Arce
 
 //18. Declarar una variable con referencia a la funcion definida
 //anteriormente (ejercicio 17)  y mostrar en consola la ejecucion de esa variable
+// Sergio Barrios
 
 //19. Declarar una variable con la fecha de hoy, agregar 30 dias a la fecha y
 //mostrar en consola el resultado
+// Jose Campero
 
 //20. Declarar una funcion probarArgumentos(argumentos) que utilice el objeto
 //arguments de la función para mostrar en consola todos los valores de argumentos
 //concatenados. Mostrar en consola el siguiente caso probarArgumentos(6,"abc",true)
+// Luis Chipana
 
 //21. Declarar una funcion agregarElementoArray(arreglo) que reciba un arreglo de
 //argumentos y muestre en consola la longitud del arreglo, luego que
@@ -140,29 +147,35 @@ console.log(num.toString());
 //en consola el contenido del arreglo y luego que quite el ultimo elemento de la
 //lista, luego que quite el elemento en el indice 2 y finalmente muestre en
 //consola el arreglo resultante
+// Ivan Figueroa
 
 /*
 OPERADORES ARITMETICOS Y LOGICOS
  */
 //22. Mostrar en consola y declarar una funcion que calcule la potencia de una base
 //y un exponente
+//Renato Francachs
 
 //23. Mostrar en consola y declarar una funcion que calcule si un numero es par o
 //impar
+//Manuel Jimenez
 
 //24. Mostrar en consola y declarar una funcion con 3 parametros de distinto tipo
 // que devuelva la concatenación de esos parametros, de 2 formas posibles
+//Pablo Martinez
 
 //25. Mostrar en consola y declarar el resultado de una operacion ternaria
-
+// Marisol Mendieta
 /*
 SENTENCIAS SELECTIVAS Y REPETITIVAS
 */
 //26. Mostrar en consola y declarar el resultado de una sentencia selectiva
 //de 2 formas posibles
+// Robert Miranda
 
 //27. Mostrar en consola y declarar el resultado de una sentencia repetitiva
 //de 4 formas posibles
+//  Joaquin Ponce
 
 /*
 TIPOS DE REFERENCIA
@@ -170,8 +183,10 @@ TIPOS DE REFERENCIA
 
 //28. Crear una clase Persona con un constructor que reciba el nombre y apellido
 //las propiedades nombre y apellido y un metodo nombreCompleto que concatene las dos propiedades
+// Richard Quispe
 
 //29. Instanciar la clase Persona con el nombre de usted como alumno y mostrar el resultado en consola
+// Michael Titizano
 
 /*
 FUNCIONES ANONIMAS
@@ -180,3 +195,4 @@ FUNCIONES ANONIMAS
 //30. Declarar una variables con una funcion anonima con un parametro y
 // otra funcion anonima con 2 parametros, mostrar los resultados
 // de ejecutar ambas variables
+// Karen Vargas
